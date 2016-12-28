4abfdc533e72f38c1e5e083699bc986e2162d6c7
AlexMili <AlexMili@users.noreply.github.com>
2016-12-28 15:17:54 +0100

    re-organization & new funcs

--- misc.py
+++ misc.py
@@ -4,8 +4,13 @@ import re
 def print_func(x):
 	print(x)
 
+# Apply regex patterns on a string
 def apply_patterns(string,patterns):
 	for pattern in patterns:
 		string = pattern.sub("",string)
 
 	return string
+
+# Keys becomes values and vis versa in a dict
+def invert_keys_values(mydict):
+	return {v: k for k, v in mydict.items()}
